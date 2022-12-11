4cc0738b1d0aeeff2cee3ece7ca5079d555cd39f
Robert Kowalski <rgkmg25@gmail.com>
2022-12-11 00:00:53 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 
 import assert from 'node:assert';
 import BAM from 'index.js';
+
+
+assert.deepEqual(
+  BAM([[ true, false, true ], [ false, true, false ]],
+  [ true, true, true ]
+);
+
+assert.deepEqual(
+  BAM([[ true, true, true ], [ false, false, false ]],
+  [ true, true, true ]
+);
