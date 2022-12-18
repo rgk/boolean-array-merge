ead570e52f9a3bb3d13d79ea1fe79ed100ed64b5
Robert Kowalski <rgkmg25@gmail.com>
2022-12-17 23:48:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-export default function BAM(arrays, dominateValue = true, result = arrays.pop(), keys = result.keys()) {
-  const current = arrays.pop();
-
-  for (let i = 0; i < keys.length; i++) {
-    if (current[keys[i]] !== dominateValue) continue;
-    const key = keys.splice(i, 1);
-    result[key] = current[key];
+export default function BAM(arrays, dominateValue = true) {
+  const result = array.pop();
+  const keys = result.keys().filter(value => value !== dominateValue);
+  for (let i = 0; i < arrays.length; i++) {
+    for (let j = 0, current = arrays.pop(); j < keys.length; j++) {
+      if (current[keys[j]] !== dominateValue) continue;
+      const key = keys.splice(j, 1);
+      result[key] = current[key];
+    }
+    
+    if (!keys.length) break;
   }
-
-  if (keys.length) BAM(arrays, dominateValue, result, keys);
-
+  
   return result;
 }
