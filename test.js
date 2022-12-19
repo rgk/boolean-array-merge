9434c1fa8eb3105d9807820aa9e831b2dcc4657e
Robert Kowalski <rgkmg25@gmail.com>
2022-12-19 00:44:50 -0500

    Update test.js

--- test.js
+++ test.js
@@ -33,8 +33,9 @@ assert.deepEqual(
 assert.deepEqual(
   BAM(
     [
-      [ false, true, false, true ], [ false, false, true ], [ false, false, false ]
-    ]
+      [ false, true, false ], [ true, false, true ]
+    ],
+    false
   ),
-  [ false, true, true, true ]
+  [ false, false, false ]
 );
