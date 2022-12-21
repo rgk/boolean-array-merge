e7694efba63b8959e040d9920b9dba0f83b51268
Robert Kowalski <rgkmg25@gmail.com>
2022-12-20 22:50:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default function BAM(arrays, dominateValue = true) {
   if (dominateValue !== true) dominateValue = false;
 
   const result = arrays.pop();
-  const keys = [ ...result.keys() ].filter(key => result[key] !== dominateValue);
+  const keys = [ ...result.keys() ].filter(key => result[key] != dominateValue);
   for (let i = 0; i < arrays.length && keys.length; i++) {
     for (let j = 0, current = arrays[i]; j < keys.length; j++) {
       if (current[keys[j]] !== dominateValue) continue;
