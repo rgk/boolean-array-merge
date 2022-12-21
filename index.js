5abae52abd2559cc24816f34fa8e08d6e5668d16
Robert Kowalski <rgkmg25@gmail.com>
2022-12-20 22:47:06 -0500

    Make sure values are boolean.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 export default function BAM(arrays, dominateValue = true) {
+  if (dominateValue !== true) dominateValue = false;
+
   const result = arrays.pop();
   const keys = [ ...result.keys() ].filter(key => result[key] !== dominateValue);
   for (let i = 0; i < arrays.length && keys.length; i++) {
     for (let j = 0, current = arrays[i]; j < keys.length; j++) {
       if (current[keys[j]] !== dominateValue) continue;
       const key = keys.splice(j--, 1)[0];
-      result[key] = current[key];
+      result[key] = current[key] ? true : false;
     }
   }
   
