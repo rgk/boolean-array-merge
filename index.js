7084e1f50e816a241d401047f315dfc344f76952
Robert Kowalski <rgkmg25@gmail.com>
2022-12-23 23:25:05 -0500

    Add comments.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-export default function BAM(arrays, dominateValue = true) {
-  dominateValue = !!dominateValue;
+export default function BAM(arrays, mergeValue = true) {
+  // Make sure merge value is always boolean.
+  mergeValue = !!mergeValue;
 
+  // Use pop to get starting set, make sure the values are boolean.
   const result = arrays.pop().map(value => !!value);
-  const keys = [ ...result.keys() ].filter(key => result[key] != dominateValue);
+
+  // Produce an array which contains the keys which should be set.
+  const keys = [ ...result.keys() ].filter(key => !!result[key] !== mergeValue);
+
+  // Loop through all arrays unless nothing is left.
   for (let i = 0; i < arrays.length && keys.length; i++) {
+    // Only go through values which need to be checked.
     for (let j = 0, current = arrays[i]; j < keys.length; j++) {
-      if (!!current[keys[j]] != dominateValue) continue;
+      // If current value being checked does not set a change.
+      if (!!current[keys[j]] !== mergeValue) continue;
+      // Remove key so it is no longer checked.
       const key = keys.splice(j--, 1)[0];
+      // Set it and make sure its boolean.
       result[key] = !!current[key];
     }
   }
