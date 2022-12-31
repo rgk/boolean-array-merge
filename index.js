869f97a7680038d82dfae5269cf68f27affd37fe
Robert Kowalski <rgkmg25@gmail.com>
2022-12-31 00:02:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ export default function BAM(arrays, mergeValue = true) {
   // Loop through all arrays unless nothing is left.
   for (let i = 0; i < arrays.length && keys.length; i++) {
     // Only go through values which need to be checked.
-    keys = keys.filter(key => (
+    keys = keys.filter(key => {
       // If current value being checked does not set a change.
       if (arrays[i][key] !== mergeValue) return true;
       // Set it and make sure its boolean.
       result[key] = !!arrays[i][key];
       // Remove key so it is no longer checked.
       return false;
-    ));
+    });
   }
 
   return result;
