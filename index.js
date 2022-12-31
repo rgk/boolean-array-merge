d3b0236b090bdd24035b95566ffa9f05225c077b
Robert Kowalski <rgkmg25@gmail.com>
2022-12-30 23:58:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ export default function BAM(arrays, mergeValue = true) {
   const result = arrays.pop().map(value => !!value);
 
   // Convert result to an array which contains the keys that will be checked.
-  const keys = [ ...result.keys() ].filter(key => result[key] !== mergeValue);
+  let keys = [ ...result.keys() ].filter(key => result[key] !== mergeValue);
 
   // Loop through all arrays unless nothing is left.
   for (let i = 0; i < arrays.length && keys.length; i++) {
     // Only go through values which need to be checked.
-    for (let j = 0, current = arrays[i]; j < keys.length; j++) {
+    keys = keys.filter(key => (
       // If current value being checked does not set a change.
-      if (!!current[keys[j]] !== mergeValue) continue;
-      // Remove key so it is no longer checked and reduce the count by 1 for the splice removal.
-      const key = keys.splice(j--, 1)[0];
+      if (arrays[i][key] !== mergeValue) return true;
       // Set it and make sure its boolean.
-      result[key] = !!current[key];
+      result[key] = !!arrays[i][key];
+      // Remove key so it is no longer checked.
+      return false;
     }
   }
 
