7546667e24c785297c43decbecfec26d2db7cb02
Robert Kowalski <rgkmg25@gmail.com>
2023-01-11 21:24:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export default function BAM(arrays, mergeValue = true, ignore = []) {
   // Make sure merge value is always boolean.
   mergeValue = !!mergeValue;
 
-  // Use pop to get starting set, make sure the values are boolean.
+  // Get starting set, make sure the values are boolean.
   const result = arrays[0].map(value => !!value);
 
   // Convert result to an array which contains the keys that will be checked.
@@ -10,7 +10,7 @@ export default function BAM(arrays, mergeValue = true, ignore = []) {
 
   // Loop through the keys so arrays don't need to be altered, fastest option.
   for (let i = 0, key = keys[i]; i < keys.length; key = keys[++i]) {
-    // Loop through the arrays last to first.
+    // Loop through the arrays first to last.
     for (let j = 0, value; ++j < arrays.length;) {
       // If current value being checked does not set a change, skip.
       if ((value = !!arrays[j][key]) !== mergeValue) continue;
