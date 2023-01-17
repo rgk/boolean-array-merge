54bec980b2a6830047c0f944741bf53b94c52abd
Robert Kowalski <rgkmg25@gmail.com>
2023-01-16 23:49:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-export default function BAM(arrays, mergeValue = true, ignore = []) {
-  // Make sure merge value is always boolean.
-  mergeValue = !!mergeValue;
-
-  // Get starting set, make sure the values are boolean.
-  const result = arrays[0].map(value => !!value);
-
-  // Convert result to an array which contains the keys that will be checked.
-  const keys = [ ...result.keys() ].filter(key => result[key] !== mergeValue && !ignore[key]);
-
-  // Loop through the keys so arrays don't need to be altered, fastest option.
-  for (let i = 0, key = keys[i]; i < keys.length; key = keys[++i]) {
-    // Loop through the arrays first to last.
-    for (let j = 0, value; ++j < arrays.length;) {
-      // If current value being checked does not set a change, skip.
-      if ((value = !!arrays[j][key]) !== mergeValue) continue;
-
-      // Set it.
-      result[key] = value;
-
-      // Move to the next key.
-      break;
-    }
-  }
-
-  return result;
-}
-
-// Binary effort, input array length can't be bigger then 4294967295.
-function BAMbinary(arrays, or = true) {
+export default function BAM(arrays, or = true) {
   const data = {
     length: arrays[0].length,
     output: []
