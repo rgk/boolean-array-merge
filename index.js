fa93b35c1c8d919331e5e7d8971cc57b607b8738
Robert Kowalski <rgkmg25@gmail.com>
2023-01-19 13:54:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Binary array merge, array input can't have more then 32 values because of JavaScript limatation with bitwise operations.
 export default function BAM(arrays, or = true) {
   const data = {
-    length: arrays[0].length,
+    length: arrays[0].length, // Length is calculated by the first array input.
     output: []
   }
 
+  // Min and max values for the 32 bit integers.
   const set = { min: 0, max: Math.pow(2, data.length) - 1 };
 
+  // Set approrate calculations depending if its OR or AND.
   if (or) {
     data.calc = value => data.total | value;
     data.complete = set.max;
@@ -16,12 +19,15 @@ export default function BAM(arrays, or = true) {
     data.total = set.max;
   }
 
+  // Loop through the arrays and merge, if the value is 'complete' before all input merged, it will end.
   for (let i = 0; i < arrays.length; i++) {
     if ((data.total = data.calc(
+      // Turn the array into a integer to be merged.
       arrays[i].reduce((total, value, index) => !!value * Math.pow(2, index) + total, 0)
     )) === data.complete) break;
   }
 
+  // Convert integers into arrays based on base 2.
   for (let i = 0; i < data.length; i++) data.output[i] = !!(data.total & (1 << i));
 
   return data.output;
